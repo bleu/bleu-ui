9508a607f0c43df65c8fdf09d48f509f7197e3f1
José Ribeiro <me@joseribeiro.dev>
2023-10-26 10:55:51 -0300

    feat: make tsup compatible with tailwind

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,5 @@
+import '../dist/index.css';
+
 import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
--- tests/Example.test.tsx
+++ tests/Example.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import renderer from 'react-test-renderer';
 import { expect, it } from 'vitest';
 import { Example } from '../src';
