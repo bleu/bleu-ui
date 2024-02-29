06040daaa90fee419cbb26ef6be6cd7a8bcffbd5
José Ribeiro <me@joseribeiro.dev>
2024-02-29 11:32:48 -0300

    fix: refactor  story types issue

--- src/stories/SectionTitle.stories.tsx
+++ src/stories/SectionTitle.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryFn } from "@storybook/react";
+import { StoryFn } from "@storybook/react";
 
 import * as React from "react";
 import { SectionTitle } from "../components/SectionTitle";
@@ -6,15 +6,20 @@ import { SectionTitle } from "../components/SectionTitle";
 export default {
   title: "SectionTitle",
   component: SectionTitle,
-  argTypes: {},
-} as Meta<typeof SectionTitle>;
+  argTypes: {
+    text: { control: "text" },
+  },
+};
 
 const Template: StoryFn<typeof SectionTitle> = (args) => (
   <SectionTitle {...args} />
 );
 
-export const Primary = Template.bind({});
+export const Primary: StoryFn<typeof SectionTitle> = Template.bind({
+  text: "Section Title Content",
+});
 
 Primary.args = {
+  // @ts-ignore
   text: "Section Title Content",
 };
