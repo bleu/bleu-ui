b122f24c1ef25f4922bd39bd8e268798e7b3efa6
José Ribeiro <me@joseribeiro.dev>
2024-07-19 13:04:10 -0300

    fix: classname was not being tailwind deduped in button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -62,7 +62,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size }), className)}
         ref={ref}
         disabled={loading}
         {...props}
