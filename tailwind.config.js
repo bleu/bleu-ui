96fbc15bd81b130b30604ff8f49558a5aa47b30d
devjoaov <joaovictorass95@gmail.com>
2024-08-03 16:00:17 -0300

    refactor: rollback adding preflight true

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // important: "#bleu-ui",
   content: ["src/**/*.{ts,tsx}", "./src/stories/**/*.{js,ts,jsx,tsx,mdx}"],
   corePlugins: {
-    preflight: false,
+    preflight: true,
   },
   theme: {
     container: {
